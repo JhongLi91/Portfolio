4c223068340843ebe194bf88edc2c579a05b96c7
Jian Hong Li <lijh0901@gmail.com>
2024-06-01 01:14:35 -0400

    More fixes

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -8,10 +8,103 @@ import { FaRegFileAlt, FaRegFolderOpen, FaRegEnvelope } from 'react-icons/fa';
 import { IoPersonOutline } from 'react-icons/io5';
 
 export default function Page() {
+
+    const Text = () => {
+        const [name, setName] = useState("");
+        const [email, setEmail] = useState("");
+        const [message, setMessage] = useState("");
+
+        const sendEmail = (e:any) => {
+            e.preventDefault();
+
+            const templateParams = {
+                from_name: name,
+                from_email: email,
+                to_name: "Jian Hong Li",
+                message: message,
+            };
+            
+            emailjs.send(
+                "service_rwpdhkz",
+                "template_64ua2um",
+                templateParams,
+                "coQKsVeLVXvF6T4t7"
+            )
+            
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    console.log("message sent");
+                    alert("Your message was successfully sent :)")
+                    setName('');
+                    setEmail('');
+                    setMessage('');
+                },
+                (error) => {
+                    console.log("ERROR");
+                }
+            );
+        }
+        
+        return (
+            <>
+                <div className="flex flex-row border-b border-black p-2">
+                    <img className="p-10" src={FatCat.src}/>
+                    <div className="py-10 flex flex-col place-content-center">
+                        <p className="text-xl inline-block flex flex-col">
+                            Want to contact me? Use the form below and leave a kind message!
+                        </p>
+                    </div>
+                </div>
+                <div className="border-b border-black p-10 text-xl">
+                    <form method="post" onSubmit={(e)=>{sendEmail(e)}}>
+                        Name:
+                        <br/>
+                            <input className="border border-black" 
+                            type="text" name="user_name" value={name}
+                            onChange={(e)=>{setName(e.target.value)}}/>
+                        <br/>
+                        Email:
+                        <br/>
+                            <input className="border border-black" 
+                            type="text" name="user_email" value={email}
+                            onChange={(e)=>{setEmail(e.target.value)}}/><br/>
+                        Comment:
+                        <br/>
+                            <textarea className="border border-black" 
+                            name="message" cols={30} rows={3} value={message}
+                            onChange={(e)=>{setMessage(e.target.value)}}/><br/><br/>
+                        
+                        <input className="border border-black mr-2 p-1" type="submit" value="Send"/>
+                        <input className="border border-black ml-2 p-1" type="reset" value="Reset"/>
+                    </form>
+                </div>
+            </>
+        )
+    }
+
+
+
+    return (
+        <div>
+            <div className="wrapper">
+                <div className="fixed">
+                    <Heading/>
+                    <div className="content">
+                        <Text/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+  );
+}
+
+const Heading = () => {
+
     const [scrollTop, setScrollTop] = useState(0);
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
-
     
     const handleScroll = () => {
         setScrollTop(window.document.documentElement.scrollTop);
@@ -91,94 +184,11 @@ export default function Page() {
         )
     }
 
-    const Text = () => {
-        const [name, setName] = useState("");
-        const [email, setEmail] = useState("");
-        const [message, setMessage] = useState("");
-
-        const sendEmail = (e:any) => {
-            e.preventDefault();
-
-            const templateParams = {
-                from_name: name,
-                from_email: email,
-                to_name: "Jian Hong Li",
-                message: message,
-            };
-            
-            emailjs.send(
-                "service_rwpdhkz",
-                "template_64ua2um",
-                templateParams,
-                "coQKsVeLVXvF6T4t7"
-            )
-            
-            .then(
-                (result) => {
-                    console.log(result.text);
-                    console.log("message sent");
-                    alert("Your message was successfully sent :)")
-                    setName('');
-                    setEmail('');
-                    setMessage('');
-                },
-                (error) => {
-                    console.log("ERROR");
-                }
-            );
-        }
-        console.log(name, email, message);
-        return (
-            <>
-                <div className="flex flex-row border-b border-black p-2">
-                    <img className="p-10" width={width/3}src={FatCat.src}/>
-                    <div className="py-10 flex flex-col place-content-center">
-                        <p className="text-xl inline-block flex flex-col">
-                            Want to contact me? Use the form below and leave a kind message!
-                        </p>
-                    </div>
-                </div>
-                <div className="border-b border-black p-10 text-xl">
-                    <form method="post" onSubmit={(e)=>{sendEmail(e)}}>
-                        Name:
-                        <br/>
-                            <input className="border border-black" 
-                            type="text" name="user_name" value={name}
-                            onChange={(e)=>{setName(e.target.value)}}/>
-                        <br/>
-                        Email:
-                        <br/>
-                            <input className="border border-black" 
-                            type="text" name="user_email" value={email}
-                            onChange={(e)=>{setEmail(e.target.value)}}/><br/>
-                        Comment:
-                        <br/>
-                            <textarea className="border border-black" 
-                            name="message" cols={30} rows={3} value={message}
-                            onChange={(e)=>{setMessage(e.target.value)}}/><br/><br/>
-                        
-                        <input className="border border-black mr-2 p-1" type="submit" value="Send"/>
-                        <input className="border border-black ml-2 p-1" type="reset" value="Reset"/>
-                    </form>
-                </div>
-            </>
-        )
-    }
-
-
-
     return (
-        <div>
-            <div className="wrapper">
-                <div className="fixed">
-                    <MyName/>
-                    <Navi/>
-                    <div className="content">
-                        <Text/>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-  );
+        <>
+            <MyName/>
+            <Navi/>
+        </>
+    )
+
 }
